9dc918e7625f3c8e7ea51535747c2e362dc57081
Rushil Perera <pererarushil1940@gmail.com>
2018-09-27 14:33:59 -0400

    Renamed MainActivity to GalleryActivity

--- app/src/main/java/com/example/rushil/galleryapp/di/component/GalleryComponent.kt
+++ app/src/main/java/com/example/rushil/galleryapp/di/component/GalleryComponent.kt
@@ -1,10 +1,10 @@
 package com.example.rushil.galleryapp.di.component
 
-import com.example.rushil.galleryapp.MainActivity
+import com.example.rushil.galleryapp.gallery.GalleryActivity
 import com.example.rushil.galleryapp.di.module.GalleryModule
 import dagger.Component
 
 @Component(modules = [GalleryModule::class])
 interface GalleryComponent {
-    fun inject(activity: MainActivity)
+    fun inject(activity: GalleryActivity)
 }
--- app/src/main/java/com/example/rushil/galleryapp/gallery/GalleryActivity.kt
+++ app/src/main/java/com/example/rushil/galleryapp/gallery/GalleryActivity.kt
@@ -1,11 +1,12 @@
-package com.example.rushil.galleryapp
+package com.example.rushil.galleryapp.gallery
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.rushil.galleryapp.R
 
-class MainActivity : AppCompatActivity() {
+class GalleryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_gallery)
     }
 }
